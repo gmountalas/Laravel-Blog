294463c56776ed9b8c9d09961ff835643c0b1993
gmountalas <mountalasgeorge@gmail.com>
2020-12-07 16:47:30 +0200

    Testing Database Interactions

---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\BlogPost;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testNoBlogPostWhenNothingDatabase()
+    {
+        $response = $this->get('/posts');
+
+        $response->assertSeeText('No posts found!');
+    }
+
+    public function testSeeOneBlogPostWhereThereIsOne()
+    {
+        // Arrange
+        $post = new BlogPost();
+        $post->title = 'New title';
+        $post->content = 'Blog post content';
+        $post->save();
+
+        // Act
+        $response = $this->get('/posts');
+
+        // Assert
+        $response->assertSeeText('New title');
+
+        $this->assertDatabaseHas('blog_posts', [
+            'title' => 'New title'
+        ]);
+    }
+}
