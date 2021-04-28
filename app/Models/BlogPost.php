002e624722d1ad9515d59b3a97b03f06d23cb159
gmountalas <mountalasgeorge@gmail.com>
2021-04-28 19:58:30 +0300

    128. Soft deletes

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -4,12 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
     protected $fillable = ['title', 'content'];
 
     use HasFactory;
+
+    // Instead of deleting the record from the database, set a field on that
+    // model called deletedAt and Laravel will know the record was soft deleted
+    use SoftDeletes;
 
     public function comments()
     {
