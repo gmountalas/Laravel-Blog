333887650c46e52bf7248407731fdfb2570da000
gmountalas <mountalasgeorge@gmail.com>
2021-07-02 21:09:48 +0300

    150. Blade Components introduction

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,22 +4,20 @@
 
 @section('content')
 
-    {{-- @if ($post['is_new'])
-        <div> A new blog post! Using if</div>
-    @else
-        <div> Blog post is old! Using elseif/else</div>    
-    @endif
-
-    @unless ($post['is_new'])
-        <div>It is an old post... using unless</div>
-    @endunless --}}
-
     <h1>{{ $post->title }}</h1>
     <p>{{ $post->content }} </p> 
     <p> Added {{ $post->created_at->diffForHumans() }}</p>
     
     @if (now()->diffInMinutes($post->created_at) < 5)
-        <div class="alert alert-info">New!</div>
+        {{-- Old way --}}
+        {{-- @component('components.badge', ['type' => 'primary'])
+            New!
+        @endcomponent --}}
+        
+        {{-- Laravel 8 way --}}
+        <x-badge type="primary">
+            New
+        </x-badge>
     @endif
     
     {{-- Implement the comments list --}}
