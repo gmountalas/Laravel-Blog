858852ddf59ec6ba09449fe7175bb6adfcf57a97
gmountalas <mountalasgeorge@gmail.com>
2021-07-04 18:23:11 +0300

    162. Practical - using cache for storage implementation

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -126,7 +126,41 @@ class PostsController extends Controller
             return BlogPost::with('comments')->findOrFail($id);
         });
 
-        $counter = 0;
+        // Implement a counter for how many people are currently on o blogPost
+        $sessionId = session()->getId();
+        $counterKey = "blog-post-{$id}-counter";
+        $usersKey = "blog-post-{$id}-users";
+
+        $users = Cache::get($usersKey, []);
+        $usersUpdate = [];
+        $difference = 0;
+        $now = now();
+
+        foreach ($users as $session => $lastVisit) {
+            if ($now->diffInMinutes($lastVisit) >= 1) {
+                $difference -= 1;
+            } else {
+                $usersUpdate[$session] = $lastVisit;
+            }
+        }
+
+        // Check if current user was in the list of users that are on a
+        // blogPost, the list is fetched from the cache
+        if (!array_key_exists($sessionId, $users) 
+            || $now->diffInMinutes($users[$sessionId]) >= 1
+        ) {
+            $difference += 1;
+        }
+
+        $usersUpdate[$sessionId] = $now;
+        Cache::forever($usersKey, $usersUpdate);
+        if (!Cache::has($counterKey)) {
+            Cache::forever($counterKey, 1);
+        } else {
+            Cache::increment($counterKey, $difference);
+        }
+
+        $counter = Cache::get($counterKey);;
 
         return view('posts.show', [
             'post' => $blogPost,
