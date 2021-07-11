45e635fa3d345762a0ce76825867f8a6215bdac0
gmountalas <mountalasgeorge@gmail.com>
2021-07-11 22:28:50 +0300

    188. Practical - Displaying uploaded image and styling

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,16 +5,27 @@
 @section('content')
     <div class="row">
         <div class="col-8">
-            <h1>
+            @if ($post->image)
+                <div style="background-image: url('{{ $post->image->url() }}'); min-height: 500px; color:white; text-aling: center; background-attachment: fixed;">
+                    <h1 style="padding-top: 100px; text-shadow: 1px 2px #000">
+            @else
+                <h1>
+            @endif
                 {{ $post->title }}
                 <x-badge show="{{ now()->diffInMinutes($post->created_at) < 5  }}" type="primary">
                     Brand New BlogPost
                 </x-badge>
-            </h1>
+            @if ($post->image)
+                    </h1>
+                </div>
+            @else
+                </h1>
+            @endif
+
             <p>{{ $post->content }} </p> 
 
             {{-- <img src="{{ Storage::url($post->image->path) }}" alt=""> --}}
-            <img src="{{ $post->image->url() }}" alt="">
+            {{-- <img src="{{ $post->image->url() }}" alt=""> --}}
             
             <x-updated :date="$post->created_at" :name="$post->user->name" >
             </x-updated>
