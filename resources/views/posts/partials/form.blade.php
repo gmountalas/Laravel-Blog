d81fad979d9332bb276f022f6ecec5661c087076
gmountalas <mountalasgeorge@gmail.com>
2021-07-11 15:31:50 +0300

    182. File upload form

--- resources/views/posts/partials/form.blade.php
+++ resources/views/posts/partials/form.blade.php
@@ -1,6 +1,7 @@
 <div class="form-group">
     <label for="title">Title</label>
-    <input type="text" id="title" name="title" class="form-control" value="{{ old('title', optional($post ?? null)->title) }}">
+    <input type="text" id="title" name="title" class="form-control" 
+        value="{{ old('title', optional($post ?? null)->title) }}">
 </div>
 @error('title')
     <div class="alert alert-danger">{{ $message }}</div>
@@ -21,4 +22,8 @@
         </ul>
     </div>
 @endif --}}
+<div class="form-group">
+    <label for="title">Thumbnail</label>
+    <input type="file" name="thumbnail" class="form-control-file">
+</div>
 <x-errors></x-errors>
