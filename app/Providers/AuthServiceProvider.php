cc0cb704d53f96c9fa45921575c34360c0b91406
gmountalas <mountalasgeorge@gmail.com>
2021-07-13 23:04:53 +0300

    192. Practical- Scafolding UserController and UserPolicy, using authorizeResource

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\BlogPost;
 use App\Models\User;
 use App\Policies\BlogPostPolicy;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -18,6 +19,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
         'App\Models\BlogPost' => BlogPostPolicy::class,
+        'App\Models\User' => UserPolicy::class,
     ];
 
     /**
