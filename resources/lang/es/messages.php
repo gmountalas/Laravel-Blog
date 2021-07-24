d17022097aa61ab46d6a3146329ae2f40bd9a784
gmountalas <mountalasgeorge@gmail.com>
2021-07-24 11:28:32 +0300

    235. Translating plural forms, passing data

---
+++ resources/lang/es/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'welcome' => '¡Bienvenido a Laravel!',
+    'example_with_value' => 'Ola! :name',
+    'plural' => '{0} Aún no hay comentarios :a|{1} :count comentario :a|[2,*] :count commentarios :a',
+];
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,6 +3,14 @@
 @section('title', 'Home page')
 
 @section('content')
-    <h1>Hello World</h1>  
+    <h1>{{ __('messages.welcome') }}</h1>  
+    {{-- <h1>@lang('messages.welcome')</h1> --}}
+
+    <p>{{ __('messages.example_with_value', ['name' => 'John']) }}</p>
+
+    <p>{{ trans_choice('messages.plural', 0, ['a' => 1]) }}</p>
+    <p>{{ trans_choice('messages.plural', 1, ['a' => 'John']) }}</p>
+    <p>{{ trans_choice('messages.plural', 2, ['a' => 'John']) }}</p>
+
     <p> Hello from Home Page</p> 
 @endsection
