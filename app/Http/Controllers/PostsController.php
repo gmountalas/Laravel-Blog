246d98a55589cc1d35e45f9d16170caf523d4315
gmountalas <mountalasgeorge@gmail.com>
2021-07-24 21:00:58 +0300

    241. What is a Service and creating a custom one

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\StorePost;
 use App\Models\BlogPost;
 use App\Models\Image;
 use App\Models\User;
+use App\Services\Counter;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Gate;
@@ -129,44 +130,47 @@ class PostsController extends Controller
         });
 
         // Implement a counter for how many people are currently on o blogPost
-        $sessionId = session()->getId();
-        $counterKey = "blog-post-{$id}-counter";
-        $usersKey = "blog-post-{$id}-users";
+        // Replaced by Counter.php Service
+        // $sessionId = session()->getId();
+        // $counterKey = "blog-post-{$id}-counter";
+        // $usersKey = "blog-post-{$id}-users";
 
-        $users = Cache::tags(['blog-post'])->get($usersKey, []);
-        $usersUpdate = [];
-        $difference = 0;
-        $now = now();
+        // $users = Cache::tags(['blog-post'])->get($usersKey, []);
+        // $usersUpdate = [];
+        // $difference = 0;
+        // $now = now();
 
-        foreach ($users as $session => $lastVisit) {
-            if ($now->diffInMinutes($lastVisit) >= 1) {
-                $difference -= 1;
-            } else {
-                $usersUpdate[$session] = $lastVisit;
-            }
-        }
+        // foreach ($users as $session => $lastVisit) {
+        //     if ($now->diffInMinutes($lastVisit) >= 1) {
+        //         $difference -= 1;
+        //     } else {
+        //         $usersUpdate[$session] = $lastVisit;
+        //     }
+        // }
 
-        // Check if current user was in the list of users that are on a
-        // blogPost, the list is fetched from the cache
-        if (!array_key_exists($sessionId, $users) 
-            || $now->diffInMinutes($users[$sessionId]) >= 1
-        ) {
-            $difference += 1;
-        }
+        // // Check if current user was in the list of users that are on a
+        // // blogPost, the list is fetched from the cache
+        // if (!array_key_exists($sessionId, $users) 
+        //     || $now->diffInMinutes($users[$sessionId]) >= 1
+        // ) {
+        //     $difference += 1;
+        // }
 
-        $usersUpdate[$sessionId] = $now;
-        Cache::tags(['blog-post'])->forever($usersKey, $usersUpdate);
-        if (!Cache::tags(['blog-post'])->has($counterKey)) {
-            Cache::tags(['blog-post'])->forever($counterKey, 1);
-        } else {
-            Cache::tags(['blog-post'])->increment($counterKey, $difference);
-        }
+        // $usersUpdate[$sessionId] = $now;
+        // Cache::tags(['blog-post'])->forever($usersKey, $usersUpdate);
+        // if (!Cache::tags(['blog-post'])->has($counterKey)) {
+        //     Cache::tags(['blog-post'])->forever($counterKey, 1);
+        // } else {
+        //     Cache::tags(['blog-post'])->increment($counterKey, $difference);
+        // }
 
-        $counter = Cache::tags(['blog-post'])->get($counterKey);;
+        // $counter = Cache::tags(['blog-post'])->get($counterKey);
+
+        $counter = new Counter();
 
         return view('posts.show', [
             'post' => $blogPost,
-            'counter' =>$counter,
+            'counter' =>$counter->increment("blog-post-{$id}", ['blog-post']),
         ]);
     }
 
