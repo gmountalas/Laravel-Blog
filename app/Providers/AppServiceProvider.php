b29af66461ef7816c07865a479c292a969141cce
gmountalas <mountalasgeorge@gmail.com>
2021-07-25 21:16:25 +0300

    257. Returning a resource collection and response wrapping

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,11 +4,13 @@ namespace App\Providers;
 
 use App\Contracts\CounterContract;
 use App\Http\ViewComposers\ActivityComposer;
+use App\Http\Resources\Comment as CommentResource;
 use App\Models\BlogPost;
 use App\Models\Comment;
 use App\Observers\BlogPostObserver;
 use App\Observers\CommentObserver;
 use App\Services\Counter;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -51,6 +53,10 @@ class AppServiceProvider extends ServiceProvider
             CounterContract::class,
             Counter::class
         );
+
+        // To return a resourse without the 'data' key wrapping the json response
+        // CommentResource::withoutWrapping();
+        JsonResource::withoutWrapping();
 
         // The Services inside the Service Container might need primitive values
         // or Classes, which are also Services, or both. In this case since we 
