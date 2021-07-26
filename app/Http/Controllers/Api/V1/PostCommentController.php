6b835092f353e222079b0ca9e439cfab4e01bd7e
gmountalas <mountalasgeorge@gmail.com>
2021-07-26 22:34:06 +0300

    262. Returning one, updating and deleting resources

--- app/Http/Controllers/Api/V1/PostCommentController.php
+++ app/Http/Controllers/Api/V1/PostCommentController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreComment;
 use App\Http\Resources\Comment as CommentResource;
 use App\Models\BlogPost;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class PostCommentController extends Controller
@@ -58,9 +59,9 @@ class PostCommentController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(BlogPost $post, Comment $comment)
     {
-        //
+        return new CommentResource($comment);
     }
 
     /**
@@ -70,9 +71,12 @@ class PostCommentController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(BlogPost $post, Comment $comment, StoreComment $request)
     {
-        //
+        $comment->content = $request->input('content');
+        $comment->save();
+
+        return new CommentResource($comment);
     }
 
     /**
@@ -81,8 +85,10 @@ class PostCommentController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(BlogPost $post, Comment $comment)
     {
-        //
+        $comment->delete();
+
+        return response()->noContent();
     }
 }
